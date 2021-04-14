99fabc868537b985d5191085db8ea80ebbd4dda8
Jan200101 <sentrycraft123@gmail.com>
2021-04-14 19:27:18 +0200

    move cmd output from an array to pointers and allocate help on heap

--- src/cmd.c
+++ src/cmd.c
@@ -6,6 +6,7 @@
 #include <psp2/kernel/modulemgr.h>
 #include <stdbool.h>
 #include <vitasdk.h>
+#include <stdlib.h>
 
 #define CMD_PORT 1338
 #define ARG_MAX (20)
@@ -17,26 +18,38 @@ extern int net_connected;
 static SceUID loader_thid;
 static int loader_sockfd;
 
-void cmd_handle(char* cmd, unsigned int cmd_size, char* res_msg)
+typedef struct
+{
+    char* ptr;
+    int alloced;
+} string_pointer;
+
+string_pointer cmd_handle(char* cmd, unsigned int cmd_size)
 {
     char* arg_list[ARG_MAX];
+    string_pointer retval;
 
     size_t arg_count = parse_cmd(cmd, cmd_size, arg_list, ARG_MAX);
     const cmd_definition* cmd_def = cmd_get_definition(arg_list[0]);
 
     if (cmd_def == NULL)
     {
-        strcpy(res_msg, "Error: Unknown command.\n");
-        return;
+        retval.ptr = "Error: Unknown command.\n";
+        retval.alloced = 0;
+        return retval;
     }
 
     if (cmd_def->arg_count != arg_count - 1)
     {
-        strcpy(res_msg, "Error: Incorrect number of arguments.\n");
-        return;
+        retval.ptr = "Error: Incorrect number of arguments.\n";
+        retval.alloced = 0;
+        return retval;
     }
 
-    cmd_def->executor(arg_list, arg_count, res_msg);
+    retval.alloced = cmd_def->alloced;
+    retval.ptr = cmd_def->executor(arg_list, arg_count);
+
+    return retval;
 }
 
 int cmd_thread(unsigned int args, void* argp)
@@ -65,13 +78,18 @@ int cmd_thread(unsigned int args, void* argp)
             char cmd[100] = { 0 };
             int size = sceNetRecv(client_sockfd, cmd, sizeof(cmd), 0);
 
-            char res_msg[60] = { 0 };
+            string_pointer res_msg;
 
             if (size >= 0)
-                cmd_handle(cmd, (unsigned int)size, res_msg);
+                res_msg = cmd_handle(cmd, (unsigned int)size);
 
-            sceNetSend(client_sockfd, res_msg, strlen(res_msg), 0);
+            sceNetSend(client_sockfd, res_msg.ptr, strlen(res_msg.ptr), 0);
             sceNetSocketClose(client_sockfd);
+
+            if (res_msg.alloced)
+            {
+                free(res_msg.ptr);
+            }
         }
         else
         {
--- src/cmd_definitions.c
+++ src/cmd_definitions.c
@@ -4,15 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <vitasdk.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define COUNT_OF(arr) (sizeof(arr) / sizeof(arr[0]))
 
 const cmd_definition cmd_definitions[] = {
-    {.name = "help", .description = "Display this help screen", .arg_count = 0, .executor = &cmd_help},
-    {.name = "destroy", .description = "Kill all running applications", .arg_count = 0, .executor = &cmd_destroy},
-    {.name = "launch", .description = "Launch an app by Title ID", .arg_count = 1, .executor = &cmd_launch},
-    {.name = "reboot", .description = "Reboot the console", .arg_count = 0, .executor = &cmd_reboot},
-    {.name = "screen", .description = "Turn the screen on or off", .arg_count = 1, .executor = &cmd_screen}
+    {.name = "help",    .description = "Display this help screen",      .arg_count = 0, .executor = &cmd_help,    .alloced = 1},
+    {.name = "destroy", .description = "Kill all running applications", .arg_count = 0, .executor = &cmd_destroy, .alloced = 0},
+    {.name = "launch",  .description = "Launch an app by Title ID",     .arg_count = 1, .executor = &cmd_launch,  .alloced = 0},
+    {.name = "reboot",  .description = "Reboot the console",            .arg_count = 0, .executor = &cmd_reboot,  .alloced = 0},
+    {.name = "screen",  .description = "Turn the screen on or off",     .arg_count = 1, .executor = &cmd_screen,  .alloced = 0}
 };
 
 const cmd_definition *cmd_get_definition(char *cmd_name) {
@@ -25,60 +27,72 @@ const cmd_definition *cmd_get_definition(char *cmd_name) {
   return NULL;
 }
 
-void cmd_help(char **arg_list, size_t arg_count, char *res_msg) {
-
-  char buf[200];
+char* cmd_help(char **arg_list, size_t arg_count) {
+  char* out;
+  char* buf;
   int longest_cmd = 0;
+  int longest_desc = 0;
 
   for (int i = 0; i < COUNT_OF(cmd_definitions); ++i)
   {
     int cmd_length = strlen(cmd_definitions[i].name);
+    int desc_length = strlen(cmd_definitions[i].description);
 
     if (cmd_length > longest_cmd) longest_cmd = cmd_length;
+    if (desc_length > longest_desc) longest_desc = desc_length;
   }
 
-  sprintf(buf, "%-*s\t\t%s\n", longest_cmd, "Command", "Description");
-  strcpy(res_msg, buf);
+  // since we know how many characters a line will hold lets save it
+  int line_length = (longest_cmd + longest_desc + 4);
+
+  // we will store COUNT_OF(cmd_definitions) + 1 lines since the help message exists
+  out = malloc(line_length * (COUNT_OF(cmd_definitions) + 1));
+  buf = malloc(line_length);
+
+  snprintf(buf, line_length, "%-*s\t\t%s\n", longest_cmd, "Command", "Description");
+  strncpy(out, buf, line_length);
   for (int i = 0; i < COUNT_OF(cmd_definitions); ++i)
   {
-    sprintf(buf, "%-*s\t\t%s\n", longest_cmd, cmd_definitions[i].name, cmd_definitions[i].description);
-    strcat(res_msg, buf);
+    snprintf(buf, line_length, "%-*s\t\t%s\n", longest_cmd, cmd_definitions[i].name, cmd_definitions[i].description);
+    strncat(out, buf, line_length);
   }
 
+  free(buf);
+
+  return out;
 }
 
-void cmd_destroy(char **arg_list, size_t arg_count, char *res_msg) {
+char* cmd_destroy(char **arg_list, size_t arg_count) {
   sceAppMgrDestroyOtherApp();
-  strcpy(res_msg, "Apps destroyed.\n");
+  return "Apps destroyed.\n";
 }
 
-void cmd_launch(char **arg_list, size_t arg_count, char *res_msg) {
+char* cmd_launch(char **arg_list, size_t arg_count) {
   char uri[32];
 
   snprintf(uri, 32, "psgm:play?titleid=%s", arg_list[1]);
 
-  if (sceAppMgrLaunchAppByUri(0x20000, uri) < 0) {
-    strcpy(res_msg, "Error: cannot launch the app. Is the TITLEID correct?\n");
-  } else {
-    strcpy(res_msg, "Launched.\n");
-  }
+  if (sceAppMgrLaunchAppByUri(0x20000, uri) < 0)
+    return "Error: cannot launch the app. Is the TITLEID correct?\n";
+
+  return "Launched.\n";
 }
 
-void cmd_reboot(char **arg_list, size_t arg_count, char *res_msg) {
+char* cmd_reboot(char **arg_list, size_t arg_count) {
   scePowerRequestColdReset();
-  strcpy(res_msg, "Rebooting...\n");
+  return "Rebooting...\n";
 }
 
-void cmd_screen(char **arg_list, size_t arg_count, char *res_msg) {
+char* cmd_screen(char **arg_list, size_t arg_count) {
   char *state = arg_list[1];
 
   if (!strcmp(state, "on")) {
     scePowerRequestDisplayOn();
-    strcpy(res_msg, "Turning display on...\n");
+    return "Turning display on...\n";
   } else if (!strcmp(state, "off")) {
     scePowerRequestDisplayOff();
-    strcpy(res_msg, "Turning display off...\n");
-  } else {
-    strcpy(res_msg, "Error: param should be 'on' or 'off'\n");
+    return "Turning display off...\n";
   }
+
+  return "Error: param should be 'on' or 'off'\n";
 }
